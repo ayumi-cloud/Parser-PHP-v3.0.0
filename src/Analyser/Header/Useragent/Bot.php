29d80984cf257ea60f025b0c92497caf1f4e288d
Ayumi Hamasaki <ayumi@github.com>
2020-11-16 13:51:58 +0000

    Add `Yahoo!` missing regex and upgrade to using oop php

--- src/Analyser/Header/Useragent/Bot.php
+++ src/Analyser/Header/Useragent/Bot.php
@@ -4,6 +4,7 @@ namespace WhichBrowser\Analyser\Header\Useragent;
 
 use WhichBrowser\Constants;
 use WhichBrowser\Data;
+use WhichBrowser\SearchEngines\Yahoo;
 
 trait Bot
 {
@@ -40,6 +41,19 @@ trait Bot
             $this->data->device->reset();
 
             $this->data->device->type = Constants\DeviceType::BOT;
+        }
+
+        /* Detect yahoo search engine bots */
+
+        if (preg_match('/(yahoo|Y\!J)/iu', $ua, $match)) {
+            $Yahoo = new Yahoo($ua);
+
+            // Only run if the class found a regex match
+            if ($Yahoo->found == true) {
+                $this->data->browser->name = $Yahoo->name ?? '';
+                $this->data->browser->version = $Yahoo->version ?? '';
+                $this->data->device->type = $Yahoo->bot ?? '';
+            }
         }
 
         return $this;
