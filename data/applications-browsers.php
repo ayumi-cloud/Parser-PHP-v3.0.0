34550958ada9dc1ae7f9c4d68d4677b2515410f7
Ayumi Hamasaki <ayumi@github.com>
2020-11-17 12:40:18 +0000

    Avast Secure browser

--- data/applications-browsers.php
+++ data/applications-browsers.php
@@ -36,6 +36,7 @@ Applications::$BROWSERS = [
         [ 'name' => 'Avant Browser',        'id'    => 'avant',      'regexp' =>'/Avant Browser/u' ],
         [ 'name' => 'Avant Browser',        'id'    => 'avant',      'regexp' =>'/Avant TriCore/u' ],
         [ 'name' => 'Avast SafeZone',       'id'    => 'asw',      'regexp' =>'/ASW\/([0-9.]*)/u', 'details' => 2 ],
+        [ 'name' => 'Avast Secure',         'id'    => 'aventsec',    'regexp' => '/ Secure\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                      // see: https://www.avast.com/secure-browser#pc
         [ 'name' => 'AVG Secure Browser',   'id'    => 'avg',      'regexp' =>'/ AVG\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                            // see: https://www.avg.com/secure-browser
         [ 'name' => 'Aviator',              'id'    => 'aviator',      'regexp' =>'/Aviator\/([0-9.]*)/u', 'details' => 1 ],                                              // see: https://github.com/WhiteHatSecurity/Aviator
         [ 'name' => 'Avira Scout',          'id'    => 'avira',       'regexp' => '/AviraScout\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                   // see: https://www.avira.com/en/avira-scout
