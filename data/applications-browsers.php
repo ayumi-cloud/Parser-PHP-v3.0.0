3d795b678635fd1e2cf9ca1b24b2d8c43a7d888b
Ayumi Hamasaki <ayumi@github.com>
2020-11-17 13:21:51 +0000

    UR Browser

--- data/applications-browsers.php
+++ data/applications-browsers.php
@@ -188,6 +188,8 @@ Applications::$BROWSERS = [
         [ 'name' => 'Tulip Chain',          'id'    => 'tulip',      'regexp' =>'/TulipChain\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                      // see: http://ostermiller.org/tulipchain/
         [ 'name' => 'Tungsten Browser',     'id'    => 'tungsten',      'regexp' =>'/TungstenBrowser\/([0-9.]*)/u' ],
         [ 'name' => 'UltraBrowser',         'id'    => 'ultrab',      'regexp' =>'/UltraBrowser ([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                  // see: http://www.ultrabrowser.com/
+        [ 'name' => 'UR Browser',           'id'    => 'ur',          'regexp' => '/ UR Browser\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                  // see: https://www.ur-browser.com/
+        [ 'name' => 'UR Browser',           'id'    => 'ur',          'regexp' => '/ UR\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],                          // see: https://www.ur-browser.com/
         [ 'name' => 'Usejump',              'id'    => 'usejump',      'regexp' =>'/Usejump\/([0-9.]*)/u', 'details' => 2, 'type' => Constants\DeviceType::DESKTOP ],
         [ 'name' => 'Vimprobable',          'id'    => 'vimprobable',      'regexp' =>'/Vimprobable\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],
         [ 'name' => 'Vivaldi',              'id'    => 'vivaldi',      'regexp' =>'/Vivaldi\/([0-9.]*)/u', 'details' => 2 ],
