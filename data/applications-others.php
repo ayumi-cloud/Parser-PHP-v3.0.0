1354a6185906186a345663beceee088bf21b4260
Ayumi Hamasaki <ayumi@github.com>
2020-11-17 12:37:11 +0000

    Steam Game Overlay (Gaming) Browser added version number detection and more regex

--- data/applications-others.php
+++ data/applications-others.php
@@ -141,6 +141,7 @@ Applications::$OTHERS = [
     Constants\BrowserType::APP_GAME => [
         [ 'name' => 'EA Origin',            'id'    => 'origin',      'regexp' =>'/Origin\/([0-9.]*)/u' ],
         [ 'name' => 'SecondLife',           'id'    => 'secondlife',      'regexp' =>'/SecondLife\/([0-9.]*)/u' ],
+        [ 'name' => 'Valve Steam',          'id'    => 'valve',           'regexp' => '/Valve Steam GameOverlay\/([0-9.]*)/u' ],
         [ 'name' => 'Valve Steam',          'id'    => 'valve',      'regexp' =>'/Valve Steam/u' ],
         [ 'name' => 'Raptr',                'id'    => 'raptr',      'regexp' =>'/Raptr/u' ],
     ],
