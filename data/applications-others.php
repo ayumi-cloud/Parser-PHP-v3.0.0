c816f640ef7d6771035796584dbba7967a222028
Ayumi <ayumih880@gmail.com>
2021-06-19 13:53:55 +0100

    Add Postman an api tester app to detection lists
    Just used `Postman` to create a new api and needed to whitelist their user agent.
    
    Link: https://www.postman.com/

--- data/applications-others.php
+++ data/applications-others.php
@@ -146,11 +146,12 @@ Applications::$OTHERS = [
     ],
 
     Constants\BrowserType::APP => [
-        [ 'name' => 'AliPay',               'id'    => 'alipay',      'regexp' =>'/AlipayClient\/([0-9.]*)/u', 'details' => 3 ],
-        [ 'name' => 'Cooliris',             'id'    => 'cooliris',      'regexp' =>'/Cooliris\/([0-9.]*)/u' ],
-        [ 'name' => 'Google Earth',         'id'    => 'google',      'regexp' =>'/Google Earth\/([0-9.]*)/u', 'details' => 2 ],
-        [ 'name' => 'Google Desktop',       'id'    => 'google',      'regexp' =>'/Google Desktop\/([0-9.]*)/u', 'details' => 2 ],
-        [ 'name' => 'Leechcraft',           'id'    => 'leechcraft',      'regexp' =>'/Leechcraft(?:\/([0-9.]*))?/u', 'details' => 2 ],
+        [ 'name' => 'AliPay',               'id'    => 'alipay',         'regexp' =>'/AlipayClient\/([0-9.]*)/u', 'details' => 3 ],
+        [ 'name' => 'Cooliris',             'id'    => 'cooliris',       'regexp' =>'/Cooliris\/([0-9.]*)/u' ],
+        [ 'name' => 'Google Earth',         'id'    => 'google',         'regexp' =>'/Google Earth\/([0-9.]*)/u', 'details' => 2 ],
+        [ 'name' => 'Google Desktop',       'id'    => 'google',         'regexp' =>'/Google Desktop\/([0-9.]*)/u', 'details' => 2 ],
+        [ 'name' => 'Leechcraft',           'id'    => 'leechcraft',     'regexp' =>'/Leechcraft(?:\/([0-9.]*))?/u', 'details' => 2 ],
         [ 'name' => 'Lotus Expeditor',      'id'    => 'expeditor',      'regexp' =>'/Gecko Expeditor ([0-9.]*)/u', 'details' => 3 ],
+        [ 'name' => 'Postman',              'id'    => 'postman',        'regexp' =>'/PostmanRuntime\/([0-9.]*)/u', 'type' => Constants\DeviceType::DESKTOP ],
     ]
 ];
