0800b78653a876ea8ea2f54cefffd39c52de35ec
Ayumi <ayumih880@gmail.com>
2021-06-19 15:38:13 +0100

    Add HyScore bot to detection list
    This bot is hitting our test servers.
    
    Link: https://hyscore.io/crawler/

--- data/applications-bots.php
+++ data/applications-bots.php
@@ -178,6 +178,7 @@ Applications::$BOTS = [
     [ 'name' => 'HubPages',                     'id'    => 'hubpages',      'regexp' => '/HubPages V([0-9.]*)/u' ],
     [ 'name' => 'HubSpot Crawler',              'id'    => 'hubspot',      'regexp' => '/hubspot\.com/u' ],
     [ 'name' => 'Huawei Web Cat Bot',           'id'    => 'huaweibot',    'regexp' => '/HuaweiWebCatBot\/([0-9.]+)/u' ],
+    [ 'name' => 'HyScore',                      'id'    => 'hyscore',      'regexp' => '/HyScore\/([0-9.]+)/u' ],
     [ 'name' => 'iAsk Spider',                  'id'    => 'spider',      'regexp' => '/iaskspider\/([0-9.]*)/u' ],
     [ 'name' => 'iAsk Spider',                  'id'    => 'spider',      'regexp' => '/iaskspider2/u' ],
     [ 'name' => 'Ichiro',                       'id'    => 'ichiro',      'regexp' => '/ichiro\/([0-9.]*)/u' ],
