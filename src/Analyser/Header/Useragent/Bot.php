a067de4936fffd28d1abfad898f2730c509438a8
Ayumi <ayumih880@gmail.com>
2021-06-20 20:46:44 +0100

    Update Bot.php

--- src/Analyser/Header/Useragent/Bot.php
+++ src/Analyser/Header/Useragent/Bot.php
@@ -17,6 +17,26 @@ trait Bot
 {
     private function &detectBot($ua)
     {
+        // Detect bots based on url in the UA string
+        if (preg_match('/\+https?:\/\//iu', $ua)) {
+            $this->data->browser->reset();
+            $this->data->os->reset();
+            $this->data->engine->reset();
+            $this->data->device->reset();
+
+            $this->data->device->type = Constants\DeviceType::BOT;
+        }
+
+        // Detect bots based on common markers
+        if (preg_match('/(?:Bot|Robot|Spider|Crawler)([\/\);]|$)/iu', $ua) && !preg_match('/CUBOT/iu', $ua)) {
+            $this->data->browser->reset();
+            $this->data->os->reset();
+            $this->data->engine->reset();
+            $this->data->device->reset();
+
+            $this->data->device->type = Constants\DeviceType::BOT;
+        }
+
         // Detect Async Http Client bot
         if (preg_match('/^AHC\/([0-9.]+)/ui', $ua, $match)) {
             $this->data->browser->reset();
