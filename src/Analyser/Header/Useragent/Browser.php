b8fe55e8f791152c5f96c830eda3ed6fbddf474f
Ayumi <ayumih880@gmail.com>
2021-06-23 20:12:20 +0100

    Update Browser.php

--- src/Analyser/Header/Useragent/Browser.php
+++ src/Analyser/Header/Useragent/Browser.php
@@ -426,14 +426,17 @@ trait Browser
         /* Fluid App */
 
         if (preg_match('/FluidApp Version\/([0-9]+)/u', $ua, $match)) {
-            $this->data->browser->using = new Using([ 'name' => 'Chrome', 'version' => new Version([ 'value' => $match[1], 'details' => 1 ]) ]);
+            // Convert `FluidApp Version/2155 to `FluidApp 2.1`
+            $match = $match[1][0] . '.' . $match[1][1];
+            $this->data->browser->version = new Version([ 'value' => $match ]);
+
+            if (preg_match('/Chrome\/([0-9]+)/u', $ua, $match)) {
+                $this->data->browser->using = new Using([ 'name' => 'Chrome', 'version' => new Version([ 'value' => $match[1], 'details' => 1 ]) ]);
+            }
 
             $this->data->browser->type = Constants\BrowserType::BROWSER;
             $this->data->browser->stock = false;
-            $this->data->browser->name = 'Fluid Browser';
-
-            // Convert `FluidApp Version/2155 to `FluidApp 2.1`
-            $match = $match[1][0] . '.' . $match[1][1];
+            $this->data->browser->name = 'Fluid App';
 
             $this->data->browser->channel = null;
         }
